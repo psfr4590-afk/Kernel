b4b0e878e9b908dd966f6c26a64641f73f14977b
John Petty <psfr4590@gmail.com>
2026-09-22 10:48:07 -0500

    feat: expose deterministic replay primitive

--- kernel/durability/__init__.py
+++ kernel/durability/__init__.py
@@ -2,5 +2,6 @@
 
 from .json import CanonicalJSONError, canonical_json
 from .sqlite import SQLiteEventStore
+from .replay import replay_request_state
 
-__all__ = ["CanonicalJSONError", "SQLiteEventStore", "canonical_json"]
+__all__ = ["CanonicalJSONError", "SQLiteEventStore", "canonical_json", "replay_request_state"]
