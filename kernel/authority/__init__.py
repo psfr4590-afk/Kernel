ac3a7e681c207cd7f1f824d797c296e79a9ae856
John Petty <psfr4590@gmail.com>
2026-09-22 16:22:44 -0500

    build: expose durable authorization records

--- kernel/authority/__init__.py
+++ kernel/authority/__init__.py
@@ -1,11 +1,13 @@
 """Authority and identity boundaries."""
 
+from .authorization import SQLiteAuthorizationStore
 from .revocation import RevocationRegistry, SQLiteRevocationRegistry
 from .service import AuthorizationError, enforce_authorization, issue_authorization
 
 __all__ = [
     "AuthorizationError",
     "RevocationRegistry",
+    "SQLiteAuthorizationStore",
     "SQLiteRevocationRegistry",
     "enforce_authorization",
     "issue_authorization",
