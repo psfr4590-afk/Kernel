2ed4563616be15257bf401d004219f6f06cf4a00
John Petty <psfr4590@gmail.com>
2026-09-22 16:46:44 -0500

    Export cryptographic identity boundary

--- kernel/__init__.py
+++ kernel/__init__.py
@@ -1,5 +1,6 @@
 """Kernel governed execution and durability core."""
 
+from .identity import IdentityError, IdentityEvidence, LocalCryptographicIdentityProvider, verify_evidence
 from .pipeline import process
 
 __version__ = "0.1.0"
