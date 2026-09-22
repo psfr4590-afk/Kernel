485edb6eba4df04a1f715e28f5d13ef68adaa11b
John Petty <psfr4590@gmail.com>
2026-09-22 11:10:29 -0500

    build: expose durable revocation registry

--- kernel/authority/__init__.py
+++ kernel/authority/__init__.py
@@ -1,6 +1,6 @@
 """Authority and identity boundaries."""
 
-from .revocation import RevocationRegistry
+from .revocation import RevocationRegistry, SQLiteRevocationRegistry
 from .service import AuthorizationError, enforce_authorization, issue_authorization
 
 __all__ = [
