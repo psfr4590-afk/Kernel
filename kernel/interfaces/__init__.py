7585b680ac2427507991e4af7b2596d9013272f8
John Petty <psfr4590@gmail.com>
2026-09-22 10:55:07 -0500

    fix: restore interface exports

--- kernel/interfaces/__init__.py
+++ kernel/interfaces/__init__.py
@@ -1,1 +1,5 @@
 """Public and internal interface boundaries."""
+
+from .contracts import Clock, ExecutionAdapter, GovernanceEvaluator, IdentityProvider
+
+__all__ = ["Clock", "ExecutionAdapter", "GovernanceEvaluator", "IdentityProvider"]
